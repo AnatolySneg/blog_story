12c6d33eb9e4d7135bbe8f3a6edaeff20cb984b9
AnatolySneg <AnatolySneg@gmail.com>
2024-03-10 16:31:06 +0200

    Models stuff

--- posts/models.py
+++ posts/models.py
@@ -2,8 +2,9 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 
+
 class Post(models.Model):
-    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, blank=True, null=True)
     title = models.CharField(max_length=100)
     text = models.TextField(max_length=3000)
     created_date = models.DateTimeField(default=timezone.now)
